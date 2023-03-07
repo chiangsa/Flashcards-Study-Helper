e9b108f59b1b1c450f25c7637bc6c678cad0ea64
shaun chiang <chiangsa@uci.edu>
2023-03-06 18:43:31 -0800

    added flashcard flipping and next mechanism, need images

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import './App.css';
+import Card from './components/Card'
+import Salt from './images/salt.jpeg';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+    const cards = [
+        {question:"Why isn't Hydrogen used to inflate balloons?", answer: "Hydrogen is highly reactive and explosive!", difficulty: "Medium"},
+        {question:"What metal is a liquid at room temperature?", answer:"Mercury", difficulty: "Easy"},
+        {question:"Why doesn't oil mix with water?", answer:"Oil is hydrophobic, meaning it repels water", difficulty: "Hard"},
+        {question:"What is dry ice made of?", answer:"Carbon dioxide", difficulty: "Easy"},
+        {question:"A pH of 4 is ___ times more acidic than a pH of 5", answer:"10", difficulty: "Medium"},
+        {question:"What element are diamonds made of?", answer:"Carbon", difficulty: "Easy"},
+        {question:"This element is essential for strong bones", answer:"Calcium", difficulty: "Easy"},
+        {question:"What is the first element of the periodic table?", answer:"Hydrogen", difficulty: "Easy"},
+        {question:"What is the formula for water?", answer:"H2O", difficulty: "Easy"},
+        {question:"What is the formula for salt?", answer:"NaCl", difficulty: "Easy"},
+        {question:"What is the most abundant element on Earth?", answer:"Oxygen", difficulty: "Hard"},
+        {question:"In general, metals have a low specific heat, meaning...?", answer:"They heat up quickly", difficulty: "Hard"},
+        {question:"What mechanism is reponsible for ice being less dense than water?", answer:"Hydrogen bonding", difficulty: "Hard"},
+        {question:"How many atoms are in a mol of something?", answer:"6.022*10^23", difficulty: "Hard"},
+        {question:"Saltwater boils at a higher or lower temperature than pure water?", answer:"Higher, as salt is hydrophilic and 'sticks' to water molecules", difficulty: "Hard"}    
+    ]
+    return (
+        <div>
+            <div class="topbar">
+                <h1 class="title1">Chemistry Flashcards</h1>
+                <h2 class="title1">Learn Some Cool Chemistry Related Facts!</h2>
+                <h2 class="title1">Number of Cards: 15</h2>
+            </div>
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+            <Card cards={cards} image={Salt}/>
+        </div>
+    )
+
 }
 
-export default App
+export default App;
---
+++ src/components/Card.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { useState} from "react";
+
+
+const Card = (props) => {
+    const [flipped, setFlipped] = useState(false);
+    const [text, setText] = useState("Click to Begin");
+    const [cardnum, setCardNum] = useState(0);
+
+
+    function flipCard() {
+        setFlipped(!flipped);
+        displayText();
+    }
+        
+
+    function getText() {
+        if(!flipped) {
+            return "Question: " + currentCard.question;
+        }
+        else{
+            return <>
+                <p>Answer: {currentCard.answer}</p>
+                <img className="images" src={props.image}/>
+            </>
+        }
+    }
+
+    function getDifficulty() {
+        return currentCard.difficulty;
+    }
+
+    function getDifficultyText() {
+        if(flipped) {
+            return "";
+        }
+        return currentCard.difficulty;
+    }
+
+    function nextCard() {
+        const randomNumber = Math.floor(Math.random() * props.cards.length);
+        setCardNum(randomNumber);
+        setFlipped(false);
+        displayText();
+    }
+    const currentCard = props.cards[cardnum]
+    return (
+        <div>
+          <div className={`Card ${getDifficulty()}`} onClick={flipCard}>
+            <h4>{getDifficultyText()}</h4>
+            <h5>{getText()}</h5>
+          </div>
+          <div className="nextbutton">
+            <button onClick={nextCard}>
+              Next
+            </button>
+          </div> 
+        </div>
+      );
+}
+
+export default Card;
